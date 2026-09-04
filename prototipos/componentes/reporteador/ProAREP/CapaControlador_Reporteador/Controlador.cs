a488c91d5eb28c249cb90d90f1da682fc51950f5
VelvethChavez <saraimejia1605@gmail.com>
2026-09-03 20:18:59 -0600

    Subiendo Prototipo No Funcional del componente Reporteador Velveth Chavez 0901 23 6269

---
+++ prototipos/componentes/reporteador/ProAREP/CapaControlador_Reporteador/Controlador.cs
@@ -0,0 +1,25 @@
+﻿using CapaModelo_Reporteador;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControlador_Reporteador
+{
+    public class Controlador
+    {
+
+        Sentencias sentencias = new Sentencias();
+        public DataTable llenarDgv(string nombreTabla)
+        {
+            OdbcDataAdapter daControlador = sentencias.llenarTbl(nombreTabla);
+            DataTable dtControlador = new DataTable();
+            daControlador.Fill(dtControlador);
+            return dtControlador;
+        }
+
+    }
+}
---
+++ prototipos/componentes/reporteador/ProAREP/CapaModelo_Reporteador/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_Reporteador
+{
+    public class Conexion
+    {
+
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=dbreporte;Uid=usuprueba;Pwd=TuContraseñaAqui;");
+            try
+            {
+                conn.Open();
+
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No se conecto");
+            }
+            return conn;
+        }
+
+
+        public void cerrarConexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No se Desconeto");
+            }
+        }
+
+
+    }
+}
+
