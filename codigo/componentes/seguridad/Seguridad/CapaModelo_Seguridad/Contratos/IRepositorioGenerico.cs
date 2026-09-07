dbfaf3280ec7d51e4ad9f66f77c192f47e0e2d5a
IsabelMelendezz <lmelendezp@miumg.edu.gt>
2026-09-06 19:27:39 -0600

    Agregando carpetas para la capa modelo

---
+++ codigo/componentes/seguridad/Seguridad/CapaModelo_Seguridad/Contratos/IRepositorioGenerico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//aca iran los metodos obligatorios para toda aquella clase 
+//que tenga herencia de la interfaz
+
+namespace CapaModelo_Seguridad.Contratos
+{
+    public interface IRepositorioGenerico<Entity> where Entity : class
+    {
+        //aqui ponemos los metodos
+        int Agregar(Entity entidad); // Agregar 
+        int Editar(Entity entidad); //Editar
+        int Remover(Entity entidad); // Eliminar
+        IEnumerable<Entity> GetAll(); // Listar los datos
+    }
+}
