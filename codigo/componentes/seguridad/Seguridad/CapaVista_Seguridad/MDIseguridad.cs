0465ad7b8cb6c193d348d609d5b85990571efc99
OscarMorales18 <omoralesl7@miumg.edu.gt>
2026-09-07 09:04:19 -0600

    Agregando funcionamiento del boton burguer

--- codigo/componentes/seguridad/Seguridad/CapaVista_Seguridad/MDIseguridad.cs
+++ codigo/componentes/seguridad/Seguridad/CapaVista_Seguridad/MDIseguridad.cs
@@ -111,5 +111,23 @@ namespace CapaVista_Seguridad
             FrmUsuarios frmasigusu = new FrmUsuarios();
             frmasigusu.ShowDialog();
         }
+
+        private void SeguridadBtnBurger_Click(object sender, EventArgs e)
+        {
+            if (SeguridadPnlNavegador.Width == 270)
+            {
+                SeguridadPnlNavegador.Width = 64;
+                SeguridadPnlDashboard.Location = new Point(200, 52);
+                SeguridadBtnBurger.Location = new Point(220, 13);
+                SeguridadLblUsuariosRol.Location = new Point(285, 19);
+            }
+            else
+            {
+                SeguridadPnlNavegador.Width = 270;
+                SeguridadPnlDashboard.Location = new Point(307, 52);
+                SeguridadBtnBurger.Location = new Point(323, 13);
+                SeguridadLblUsuariosRol.Location = new Point(390, 19);
+            }
+        }
     }
 }
