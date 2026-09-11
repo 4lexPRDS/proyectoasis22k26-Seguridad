a6455fd4acdfc77a42f8089434036f27446fac74
VelvethChavez <saraimejia1605@gmail.com>
2026-09-11 08:20:18 -0600

    Prototipo No Funcional sin codigo Reporteador

--- codigo/componentes/reporteador/ProAREP/CapaControlador_Reporteador/Controlador.cs
+++ codigo/componentes/reporteador/ProAREP/CapaControlador_Reporteador/Controlador.cs
@@ -1,4 +1,4 @@
-﻿using CapaModelo_Reporteador;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,15 +11,7 @@ namespace CapaControlador_Reporteador
 {
     public class Controlador
     {
-
-        Sentencias sentencias = new Sentencias();
-        public DataTable llenarDgv(string nombreTabla)
-        {
-            OdbcDataAdapter daControlador = sentencias.llenarTbl(nombreTabla);
-            DataTable dtControlador = new DataTable();
-            daControlador.Fill(dtControlador);
-            return dtControlador;
-        }
+     
 
     }
 }
