8ee286463b7e3351e5462a53e196350033b5fece
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-13 11:38:41 -0600

    Cambio de namespace de FrmAsignacionPerfiles.cs y FrmBitacora.cs

--- codigo/componentes/seguridad/Seguridad/CapaVista_Seguridad/FrmMDISeguridad.cs
+++ codigo/componentes/seguridad/Seguridad/CapaVista_Seguridad/FrmMDISeguridad.cs
@@ -1,4 +1,4 @@
-﻿using AplicacionPerfiles;
+﻿using CapaControlador_Seguridad;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Ventana_Bitacora_Seguridad;
+
 
 namespace CapaVista_Seguridad
 {
