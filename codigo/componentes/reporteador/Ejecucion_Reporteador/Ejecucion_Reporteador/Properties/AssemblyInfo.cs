11d9fc38104c950d34a4e6868e19a420d7b32315
VelvethChavez <saraimejia1605@gmail.com>
2026-09-13 22:15:42 -0600

    Cambios solicitados por Pruebas, 30% estandarizacion formulario y botones con su DLL

--- codigo/componentes/reporteador/Ejecucion_Reporteador/Ejecucion_Reporteador/Properties/AssemblyInfo.cs
+++ codigo/componentes/reporteador/Ejecucion_Reporteador/Ejecucion_Reporteador/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("3c0aae3f-cdf3-44ba-a54f-0b7ec1611de2")]
+[assembly: Guid("82ba879b-9fc9-4bad-8eb6-3bb958ef1ad4")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
