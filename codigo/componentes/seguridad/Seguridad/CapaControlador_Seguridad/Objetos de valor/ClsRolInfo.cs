786399c6277ab6016a7cc36ff5b85d30bf122db4
IsabelMelendezz <lmelendezp@miumg.edu.gt>
2026-09-24 20:18:49 -0600

    Subiendo comentarios faltantes

--- codigo/componentes/seguridad/Seguridad/CapaControlador_Seguridad/Objetos de valor/ClsRolInfo.cs
+++ codigo/componentes/seguridad/Seguridad/CapaControlador_Seguridad/Objetos de valor/ClsRolInfo.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * ==================================================================
+ * Área: Seguridad
+ * Autores: Lourdes Isabel Melendez Pineda
+ * Fecha o ultima edicion: 24/09/2026
+ * ==================================================================
+ * Propósito : Clase que guarda el id y el nombre de un rol, usada
+ * para llevar la lista de roles del usuario en sesión.
+ * ===================================================================
+ */
 namespace CapaControlador_Seguridad.Objetos_de_valor
 {
     public class ClsRolInfo
